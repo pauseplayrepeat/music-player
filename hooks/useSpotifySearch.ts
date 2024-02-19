48df8cd9f071c16192566ca97199c8307c980662
pauseplayrepeat <mrsqueex@gmail.com>
2024-02-19 03:50:52 -0600

    update usespotifysearch

--- hooks/useSpotifySearch.ts
+++ hooks/useSpotifySearch.ts
@@ -11,22 +11,22 @@ const useSpotifySearch = (query: string): Track[] => {
   const searchTracks = useSpotify();
   const [results, setResults] = useState<Track[]>([]);
 
-  useEffect(() => {
-    const executeSearch = async () => {
-      if (!searchTracks) {
-        console.error("SearchTracks function not found");
-        return;
-      }
-      const res = await searchTracks(query);
-      if (res && res.tracks && res.tracks.items) { 
-        setResults(res.tracks.items);
-      }
-    };
+  // useEffect(() => {
+  //   const executeSearch = async () => {
+  //     if (!searchTracks) {
+  //       console.error("SearchTracks function not found");
+  //       return;
+  //     }
+  //     const res = await searchTracks(query);
+  //     if (res && res.tracks && res.tracks.items) { 
+  //       setResults(res.tracks.items);
+  //     }
+  //   };
 
-    if (query) {
-      executeSearch();
-    }
-  }, [query, searchTracks]);
+  //   if (query) {
+  //     executeSearch();
+  //   }
+  // }, [query, searchTracks]);
 
   return results;
 };
