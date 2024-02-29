a74396799c4d5031a519be6e1544bce8e99f4ad1
pauseplayrepeat <mrsqueex@gmail.com>
2024-02-29 08:29:31 -0600

    updating

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import EmailButton from "./components/EmailButton";
 import SpotifyTrackList from './components/SpotifyTrackList';
 import getSpotifyTracksByUserId from '@/actions/getSpotifyTracksByUserId';
 import { SpotifyTrack } from '@/types';
+import Header from '@/components/Header';
+import SpotifyTrackUserItem from './components/SpotifyTrackUserItem';
 
 // interface DashboardPageProps {
 //   spotifyTracks: SpotifyTrack[];
@@ -12,23 +14,57 @@ import { SpotifyTrack } from '@/types';
 const Dashboard = ({ 
     // spotifyTracks,
 }) => {
-  return (
-    <div 
-      className="
-        grid 
-        grid-cols-2 
-        sm:grid-cols-3 
-        md:grid-cols-3 
-        lg:grid-cols-4 
-        xl:grid-cols-5 
-        2xl:grid-cols-8 
-        gap-4 
-      "
-    >
-      {/* <EmailButton /> */}
-      <SpotifyTrackList />
-    </div>
-  );
-};
+    return (
+        <div
+        className="
+          bg-neutral-900 
+          rounded-lg 
+          h-full 
+          w-full 
+          overflow-hidden 
+          overflow-y-auto
+        "
+      >
+         <Header>
+            <div className="mb-2">
+              <h1 
+                className="
+                text-white 
+                  text-3xl 
+                  font-semibold
+                ">
+                  Send your music for promotion
+              </h1>
+              <div 
+                className="
+                  grid 
+                  grid-cols-1 
+                  sm:grid-cols-1 
+                  xl:grid-cols-2
+                  2xl:grid-cols-2
+                  gap-3 
+                  mt-4
+                "
+              >
+                Simply click send email to send an email to music promoters
+              </div>
+            </div>
+          </Header>
+          <div 
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-6
+              2xl:grid-cols-5
+              gap-3 
+              mt-4
+            "
+          >
+            <SpotifyTrackList />
+        </div>
+        </div>
+      );
+    };
 
 export default Dashboard; // Make sure to export Dashboard as default
