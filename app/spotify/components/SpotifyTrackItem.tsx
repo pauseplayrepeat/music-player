3afc2a969beea12b230635e5f9cbedc02d063f7d
pauseplayrepeat <mrsqueex@gmail.com>
2024-03-01 00:50:27 -0600

    reverted back to going to spotify url on homepage and incrementing the click

--- app/spotify/components/SpotifyTrackItem.tsx
+++ app/spotify/components/SpotifyTrackItem.tsx
@@ -18,44 +18,44 @@ const SpotifyTrackItem: React.FC<SpotifyTrackItemProps> = ({ track }) => {
     }
  
     // Function to handle click event
-    // const handleClick = async () => {
-    //     // Ensure track_url is not null or undefined before attempting to redirect
-    //     if (track.track_url) {
-    //         // Increment click_count in the database
-    //         try {
-    //             const { data, error: fetchError } = await supabaseClient
-    //                 .from('spotify_tracks')
-    //                 .select('click_count')
-    //                 .eq('id', track.id)
-    //                 .single();
-            
-    //             if (fetchError) throw fetchError;
-            
-    //             const { error: updateError } = await supabaseClient
-    //                 .from('spotify_tracks')
-    //                 .update({ click_count: (data.click_count + 1) })
-    //                 .match({ id: track.id });
-            
-    //             if (updateError) throw updateError;
-    //         } catch (error) {
-    //             console.error('Error incrementing click count:', error);
-    //         }
-
-    //         window.location.href = track.track_url; // Redirect to the track URL
-    //     }
-    // };
-
-
     const handleClick = async () => {
         // Ensure track_url is not null or undefined before attempting to redirect
         if (track.track_url) {
             // Increment click_count in the database
-            // ... database code
+            try {
+                const { data, error: fetchError } = await supabaseClient
+                    .from('spotify_tracks')
+                    .select('click_count')
+                    .eq('id', track.id)
+                    .single();
+            
+                if (fetchError) throw fetchError;
+            
+                const { error: updateError } = await supabaseClient
+                    .from('spotify_tracks')
+                    .update({ click_count: (data.click_count + 1) })
+                    .match({ id: track.id });
+            
+                if (updateError) throw updateError;
+            } catch (error) {
+                console.error('Error incrementing click count:', error);
+            }
 
-            // Navigate to the track page
-            router.push(`/tracks/${track.id}`);
+            window.location.href = track.track_url; // Redirect to the track URL
         }
     };
+
+
+    // const handleClick = async () => {
+    //     // Ensure track_url is not null or undefined before attempting to redirect
+    //     if (track.track_url) {
+    //         // Increment click_count in the database
+    //         // ... database code
+
+    //         // Navigate to the track page
+    //         router.push(`/tracks/${track.id}`);
+    //     }
+    // };
 
     // Now that we've checked that track is defined, we can safely access its properties
     const artworkSrc = track.song_artwork || '/path/to/default/image.png'; // Provide a fallback image
