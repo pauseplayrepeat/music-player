18f98c7791e62611bf11ea06206e8f4533e47100
pauseplayrepeat <mrsqueex@gmail.com>
2024-03-01 00:41:25 -0600

    updated more stuff

--- app/tracks/tracksid/page.tsx
+++ app/tracks/tracksid/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
 import getSpotifyTracks from "@/actions/getSpotifyTracks";
-import TrackIdPageContent from "../components/TrackIdPageContent";
+import TrackIdPageContent from "./components/TrackIdPageContent";
 import fetchTrackById from "@/actions/getSpotifyTracksByTrackId";
 
 import { useParams } from "next/navigation";
